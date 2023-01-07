02989680f688d77fc56feef3385c757ed8589a39
Shweta-MG <shweta.malav@gmail.com>
2023-01-07 23:31:35 +0100

    general greeting messages are updated

--- javascript/javascript1/week4/voiceAssistance.js
+++ javascript/javascript1/week4/voiceAssistance.js
@@ -5,3 +5,12 @@ let a = 10;
 let b = 12;
 
 function getReply(command){
+
+//general greeting message
+if (command.includes ("hello", "hey")) {
+    return "Hello! Welcome home."
+} 
+
+else if (command.includes("I am ")){
+    return "Hello! Nice to meet you."
+}
