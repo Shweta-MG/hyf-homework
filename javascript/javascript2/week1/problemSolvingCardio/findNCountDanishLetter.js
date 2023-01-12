a3bc064c6ad473cdee17101e1949853df4e48bc3
Shweta-MG <shweta.malav@gmail.com>
2023-01-12 19:40:56 +0100

    file updated.

--- javascript/javascript2/week1/problemSolvingCardio/findNCountDanishLetter.js
+++ javascript/javascript2/week1/problemSolvingCardio/findNCountDanishLetter.js
@@ -1,8 +1,6 @@
-const danishString = "Jeg har en blå bil";
-//notThisFunctionName(danishString); // returns {total: 1, å: 1}
+const danishString = "Jeg har en blå bil"; 
 
-const danishString2 = "Blå grød med røde bær";
-//notThisFunctionName(danishString2); // returns {total: 4, æ: 1, ø: 2, å: 1}
+const danishString2 = "Blå grød med røde bær"; 
 
 const danishString3 = "19-årige Jakob fra København skal spille VM for USA";
 
