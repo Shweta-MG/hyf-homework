06abdf38e60ea0b4486393b593813c54e01c2c0d
Shweta-MG <shweta.malav@gmail.com>
2023-01-12 14:47:37 +0100

    Initial codes weer updated.

--- javascript/javascript2/week1/problemSolvingCardio/findTheShortestWord.js
+++ javascript/javascript2/week1/problemSolvingCardio/findTheShortestWord.js
@@ -14,3 +14,28 @@ console.log(shortesdWord[0]);
 
 //get longest word in the list
 console.log(shortesdWord[shortesdWord.length - 1]);
+
+
+/* 
+
+I wrote this programme and for some reason, it is giving me 'bil' as shortest word.
+
+
+function shortesdWord(danishWords) {
+  let smallestStr = danishWords[0].length
+  let smallestDanishWord = danishWords[0]
+  for (let j = 0; j < danishWords.length; j++){
+    let smallestdanskOrd = danishWords[j].length;
+    if (smallestdanskOrd < smallestStr){
+      smallestDanishWord = danishWords[j];
+      smallestStr = smallestDanishWord ;
+    }
+    return smallestDanishWord;
+  }
+}
+
+console.log(shortesdWord(danishWords));
+
+*/
+
+
