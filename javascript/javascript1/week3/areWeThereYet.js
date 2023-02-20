a22d5439f5f73fc8aeb36a936ee78bced4191fc9
Shweta <108921644+Shweta-MG@users.noreply.github.com>
2023-02-20 22:23:02 +0100

    Update areWeThereYet.js

--- javascript/javascript1/week3/areWeThereYet.js
+++ javascript/javascript1/week3/areWeThereYet.js
@@ -21,3 +21,33 @@ function toHoursAndMinutes() { if (totalTravelTime < 60) {
     }    
 }
 toHoursAndMinutes();
+
+
+//This solution is updated as per comments
+
+const travelInformation = {
+  speed: 50,
+  destinationDistance: 432,
+};
+
+const travelInformation = {
+    speed: 50,
+    destinationDistance: 432,
+};
+
+                                       
+/* Let's calculate total travel time in advance. I think, this will make codes easy to understand. */
+let totalTravelTime = ((travelInformation.speed)/60) * (travelInformation.destinationDistance);
+const travelTime = notThisFunctionName(travelInformation)
+/*Considering that the same codes might be used for other travel time estimation, I am writing codes for more than and less than an hour long travel. */
+
+function notThisFunctionName(travelInformation) { if (totalTravelTime < 60) {
+    return ("You will reach distination in " + totalTravelTime + " minutes.");
+    } else { const hours = Math.floor( totalTravelTime/ 60);
+    const minutes = Math.floor( totalTravelTime % 60);
+    return ("You will reach distination in " + hours + " hr " + minutes + " mins.")
+
+    }    
+}
+console.log(travelTime);
+
