7ce3212971a13686cc27f4c6f402db5a85c6097b
Shweta <108921644+Shweta-MG@users.noreply.github.com>
2023-03-29 20:12:45 +0200

    Updated as per Mentor's comments

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -2,13 +2,13 @@
 let average = 0;
 let sum = 0;
 
-if(process.argv.length === 2){
+if(process.argv.length <= 2){
     console.log('Please enter valid input'); 
     return
 } else {
     for (let j = 2; j < process.argv.length; j++) {
         // console.log(j + ' -> ' + (process.argv[j]));
-        if (typeof (parseInt(process.argv[j])) !== 'number') {
+        if (typeof (parseInt(process.argv[j])) !== 'number' || parseInt(process.argv[j])) !== 'NaN') {
             console.log ('Please enter valid input number');
         } else {
             sum += parseInt(process.argv[j]);
