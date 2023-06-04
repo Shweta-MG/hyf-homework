c90ee645dabf27f69ab3be56f34ef8f13ba32a51
Shweta-MG <shweta.malav@gmail.com>
2023-06-04 21:13:01 +0200

    Old codes were revised.

--- javascript/javascript1/week1/ezNamey.js
+++ javascript/javascript1/week1/ezNamey.js
@@ -1,8 +1,44 @@
 //Ez Namey (Startup name generator) Optional
-let firstWords = [ "Øl", "kaffe", "hygge", "solen", "vin", "viking", "Danmark", "dansk", "venlig", "varme" ];
-let secondWords = ["velkommende", "læring", "indblik", "hårdarbejde", "qarma", "danskprøve", "skøn", "dejlig", "lækert", "glad"];
-let randonFirstWords = firstWords[Math.floor(Math.random() * 10)];
-let randonSecondWords = secondWords[Math.floor(Math.random() * 10)];
-let startupName = randonFirstWords + " " + randonSecondWords;
-startupNameLength = startupName.length
-console.log("'The startup: " + "\'" + startupName + "\'" +" contains " + startupNameLength + " characters.'");
+let firstWords = ["Øl",
+    "kaffe",
+    "hygge",
+    "solen",
+    "vin",
+    "viking",
+    "Danmark",
+    "dansk",
+    "venlig",
+    "varme"
+];
+
+let secondWords = ["velkommende",
+    "læring",
+    "indblik",
+    "hårdarbejde",
+    "qarma",
+    "danskprøve",
+    "skøn",
+    "dejlig",
+    "lækert",
+    "glad"
+];
+
+let randomFirstWords = firstWords[Math.floor(Math.random() * 10)];
+
+let randomSecondWords = secondWords[Math.floor(Math.random() * 10)];
+
+let startupName = `${randomFirstWords} ${randomSecondWords}`;
+
+let startupNameLength = startupName.length
+
+let print = `The startup: '${startupName}' contains ${startupNameLength} characters.`
+
+console.log(print);
+
+
+/*
+console.log("'The startup: " + "\'" + startupName + "\'" + " contains " + startupNameLength + " characters.'");
+
+let startupName = randomFirstWords + " " + randomSecondWords;
+*/
+
