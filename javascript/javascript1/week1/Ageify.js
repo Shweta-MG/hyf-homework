2134004b9d62315b93245b7311169e8d69a84361
Shweta-MG <shweta.malav@gmail.com>
2023-06-04 20:44:24 +0200

    Code updated in agify and warm up exericse to be updated here.

--- javascript/javascript1/week1/Ageify.js
+++ javascript/javascript1/week1/Ageify.js
@@ -1,4 +1,7 @@
 let yearOfBirth = 1987;
 let yearFuture = 2027;
 let age = (yearFuture - yearOfBirth);
-console.log( "You will be " + age + "  years old in " + yearFuture + " .");
+console.log("You will be " + age + "  years old in " + yearFuture + " .");
+
+let ageNew = `You will be ${age} years old in ${yearFuture}.`
+console.log(ageNew);
