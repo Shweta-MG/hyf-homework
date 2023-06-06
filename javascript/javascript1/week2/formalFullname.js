10b57d89f504623de522fd183ac5b5184df753b6
Shweta-MG <shweta.malav@gmail.com>
2023-06-06 20:28:35 +0200

    Better functions is written based on current knowledge.

--- javascript/javascript1/week2/formalFullname.js
+++ javascript/javascript1/week2/formalFullname.js
@@ -1,10 +1,12 @@
 /* user has the possibility to check a checkbox that indicates if the user wants to be adressed formally, and name title will be based on gender, So I decided to use 'confirm' because it is bolean, 'ok' means 'true'.*/
 
+/*
 
 let useFormalName = confirm("Do you want to be addressed formally?");
 let gender = confirm("Are you a male passenger?");
 
-/* I want to user to verify name as written in passport. So decided to an alert before this*/
+I want to user to verify name as written in passport. So decided to an alert before this
+
 let passport = alert("Make sure to refer your passport for your first name and last name.");
 
 let fullname1 = prompt("Enter your first name?", "");
@@ -18,6 +20,49 @@ if (useFormalName === true && gender === true) {
        console.log ("Lady " + getFullName);
     } else {
        console.log (getFullName);
-    } 
+} 
+    
+*/
 
 /* Calling out empty string will give them funny names. I am sure no one wants to spent money on that*/
+
+let useFormalName = true;
+let chooseGender;
+let fullname1;
+let fullname2;
+
+const getFullName = (fullname1, fullname2, useFormalName, chooseGender) => {
+   if (fullname1.trim() === '' || fullname1.trim() === '') {
+      return `Please provide valid input`;      
+   }
+   else {
+      if (useFormalName) {
+         if (chooseGender === 'Male') {
+            let fullname = `Lord ${fullname1} ${fullname2}`;
+            return fullname;
+         }
+         else if (chooseGender === 'Female')
+         {
+            let fullname = `Lady ${fullname1} ${fullname2}`;
+            return fullname;         
+         }
+         else
+         {
+            let fullname = `Dear ${fullname1} ${fullname2}`;
+            return fullname;
+         }
+      }
+      else
+      {
+         let fullname = `${fullname1} ${fullname2}`;
+         return fullname;
+      }      
+   }
+}
+
+console.log(getFullName('Sandy', 'Thomas', true, 'Male'));
+console.log(getFullName('Shweta', 'Malav', true, 'Female'));
+console.log(getFullName('Shweta', 'Malav', true));
+console.log(getFullName('Shweta', 'Malav'));
+console.log(getFullName('', '', true, 'Male'));
+console.log(getFullName(' ', ' ', true, 'Male'));
