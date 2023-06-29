0aa68684b99afac626aa0a5f1296d30aecd190e4
Shweta-MG <shweta.malav@gmail.com>
2023-06-29 12:24:25 +0200

    Codes updated as per current understanding.

--- react/react1/week2/todo-list/src/Components/ToDo/ToDoList/ToDoList.js
+++ react/react1/week2/todo-list/src/Components/ToDo/ToDoList/ToDoList.js
@@ -7,14 +7,40 @@ const ToDoList = (props) => {
     const todos = props.todos;
     const title = props.title;
     const deleteHandler = props.deleteHandler;
+
+    {/**
+    
+
+
+    const CheckedToDo = () => {
+        const [isChecked, setIsChecked] = useState(false);
+    }
+
+    const handleCheckboxChange = () => {
+        setIsChecked(!isChecked);
+    };
+    
+    const textStyle = {
+        textDecoration: isChecked ? 'line-through' : 'none'
+    };
+
+        */
+
+}
+
+
    
     return (  
         <div className="todo-list">
             <h2>{title}</h2>
             {todos.length === 0 ? 'No To-Dos to display' : todos.map((todo) => (<div className="todo-preview" key={todo.id}>
-                <div>
-                <h2> {todo.description}</h2>
+                <div className="todo-description">
+                    <input type="checkbox"
+                        name="checkbox"
+                        id="checkbox" />
+                    <h2> {todo.description}</h2>
                 </div>
+
                 <div className="todo-details">
                 <p> {todo.deadline}</p>  
                 <button onClick={() => { deleteHandler(todo.id) }}>
